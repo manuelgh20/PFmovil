9488857b4ab3999b2929c6d01b54b51aa0b47af6
Manuel Guerrero <manuelgrrr2095@gmail.com>
2022-08-12 18:58:03 -0600

    Primer Push
    1

---
+++ app/src/main/java/com/example/bancos/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.bancos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.bancos.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btLogin.setOnClickListener{haceLogin()}
+        binding.btRegister.setOnClickListener{haceRegister()}
+    }
+
+    private fun haceRegister() {
+        val email=binding.etEmail.text.toString()
+        val clave=binding.etEmail.text.toString()
+
+        //se hace registro
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful){
+                    Log.d("Autenticando","creado")
+                    val user=auth.currentUser
+                    actualiza(user)
+                }else{
+                    Log.d("Autenticando","fregistroaail")
+                    Toast.makeText(baseContext,"fallo",Toast.LENGTH_LONG).show()
+                    actualiza(null)
+
+                }}
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if(user!=null){
+            val intent= Intent(this,Principal::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    public override fun onStart(){
+        super.onStart()
+        val usuario= auth.currentUser
+        actualiza(usuario)
+
+
+    }
+
+    private fun haceLogin() {
+        val email=binding.etEmail.text.toString()
+        val clave=binding.etEmail.text.toString()
+
+        //se hace registro
+        auth.signInWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful){
+                    Log.d("Autenticando","login")
+                    val user=auth.currentUser
+                    actualiza(user)
+                }else {
+                    Log.d("Autenticando", "faail")
+                    Toast.makeText(baseContext,"fallo",Toast.LENGTH_LONG).show()
+                    actualiza(null)
+
+                }}
+    }
+}
