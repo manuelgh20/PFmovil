474d2b5dbdf3752fb3b9233803ae7d2fc5725482
Manuel Guerrero <manuelgrrr2095@gmail.com>
2022-08-15 23:24:47 -0600

    Creacion de cardview y navegacio
    Creacion de cardview y navegacion entre fragmento ademas de insert de cuenta

--- app/src/main/java/com/example/bancos/ui/banco/BancoFragment.kt
+++ app/src/main/java/com/example/bancos/ui/banco/BancoFragment.kt
@@ -7,38 +7,32 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.bancos.R
+import com.example.bancos.databinding.FragmentAddBancoBinding
 import com.example.bancos.databinding.FragmentBancoBinding
 
 import com.example.bancos.viewmodel.BancoViewModel
 
 class BancoFragment : Fragment() {
 
+    private lateinit var bancoViewModel: BancoViewModel
     private var _binding: FragmentBancoBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val bancoViewModel =
-            ViewModelProvider(this).get(BancoViewModel::class.java)
-
+        bancoViewModel = ViewModelProvider(this)[BancoViewModel::class.java]
         _binding = FragmentBancoBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        bancoViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+         binding.addLugarFabButton.setOnClickListener {
+             findNavController().navigate(R.id.action_nav_banco_to_addBancoFragment)
+         }
+             return binding.root
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
