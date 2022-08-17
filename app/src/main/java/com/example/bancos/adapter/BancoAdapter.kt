cc43ca371edefa3f690f30baa4cb62deccc98669
Manuel Guerrero <manuelgrrr2095@gmail.com>
2022-08-16 22:05:38 -0600

    Crud completo
    Se terminad de realizar el crud para poder ingresar cuentas

--- app/src/main/java/com/example/bancos/adapter/BancoAdapter.kt
+++ app/src/main/java/com/example/bancos/adapter/BancoAdapter.kt
@@ -1,25 +1,54 @@
 package com.example.bancos.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bancos.databinding.BancoFilaBinding
+import com.example.bancos.model.Banco
+import com.example.bancos.ui.banco.BancoFragmentDirections
 
 class BancoAdapter : RecyclerView.Adapter<BancoAdapter.BancoViewHolder>(){
+
+    private var listaBancos = emptyList<Banco>()
 
     inner class BancoViewHolder(private val itemBinding: BancoFilaBinding):
         RecyclerView.ViewHolder(itemBinding.root){
 
+        fun bind (banco: Banco){
+
+            itemBinding.tvCedula.text = banco.Cedula
+            itemBinding.tvNombre.text = banco.nombre
+            itemBinding.tvCorreo.text = banco.correo
+            itemBinding.tvTelefono.text = banco.telefono
+            itemBinding.tvCuenta.text = banco.cuenta
+            itemBinding.tvCodigo.text = banco.codigo
+            itemBinding.vistaFila.setOnClickListener{
+                val action=BancoFragmentDirections.actionNavBancoToUpdateBancoFragment(banco)
+                itemView.findNavController().navigate(action)
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BancoViewHolder {
-        TODO("Not yet implemented")
+        val itemBinding = BancoFilaBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,false)
+        return BancoViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(holder: BancoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val banco=listaBancos[position]
+        holder.bind(banco)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listaBancos.size
+    }
+
+    fun setdata(bancos : List<Banco>){
+        this.listaBancos=bancos
+        notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/bancos/ui/banco/UpdateBancoFragment.kt
@@ -0,0 +1,93 @@
+package com.example.bancos.ui.banco
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.bancos.R
+import com.example.bancos.databinding.FragmentAddBancoBinding
+import com.example.bancos.databinding.FragmentUpdateBancoBinding
+import com.example.bancos.model.Banco
+import com.example.bancos.viewmodel.BancoViewModel
+
+
+class UpdateBancoFragment : Fragment() {
+    private lateinit var bancoViewModel: BancoViewModel
+
+    private val args by navArgs<UpdateBancoFragmentArgs >()
+
+    private var _binding: FragmentUpdateBancoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bancoViewModel = ViewModelProvider(this)[BancoViewModel::class.java]
+        _binding = FragmentUpdateBancoBinding.inflate(inflater, container, false)
+
+        binding.etCedula.setText(args.banco.Cedula)
+        binding.etNombre.setText(args.banco.nombre)
+        binding.etCorreo.setText(args.banco.correo)
+        binding.etTelefono.setText(args.banco.telefono)
+        binding.etCuenta.setText(args.banco.cuenta)
+        binding.etCodigo.setText(args.banco.codigo)
+
+
+
+
+        binding.btActualizar.setOnClickListener{
+        updateBanco()
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu,menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId ==R.id.menu_delete) {
+            deleteLugar()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateBanco() {
+
+        val cedula=binding.etCedula.text.toString()
+        val nombre=binding.etNombre.text.toString()
+        val correo=binding.etCorreo.text.toString()
+        val telefono=binding.etTelefono.text.toString()
+        val cuenta=binding.etCuenta.text.toString()
+        val codigo=binding.etCodigo.text.toString()
+        val banco= Banco(args.banco.id,cedula,nombre,correo,telefono,cuenta,codigo)
+        bancoViewModel.updateBanco(banco)
+        Toast.makeText(requireContext(),getString(R.string.msg_actualizado),Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_updateBancoFragment_to_nav_banco)
+
+    }
+
+    private fun deleteLugar(){
+        val builder= AlertDialog.Builder(requireContext())
+        builder.setPositiveButton(getString(R.string.si)){_,_ ->
+            bancoViewModel.deleteBanco(args.banco)
+            findNavController().navigate(R.id.action_updateBancoFragment_to_nav_banco)
+        }
+        builder.setNegativeButton(getString(R.string.no)){_,_ ->}
+        builder.setTitle(R.string.menu_delete)
+        builder.setMessage(getString(R.string.msg_segurro_borrar)+"${args.banco.nombre}?")
+        builder.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
+    }
+    }
+
+
